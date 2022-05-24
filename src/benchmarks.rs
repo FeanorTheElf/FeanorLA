b9ee309dea98a0eabd3c1b668a90b876e267d7e6
Simon Pohmann <simon@pohmann.de>
2022-05-24 10:16:07 +0100

    Restructured elliptic curves

--- src/benchmarks.rs
+++ src/benchmarks.rs
@@ -17,17 +17,3 @@ fn bench_poly_multiplication(b: &mut Bencher) {
         assert_eq!(h, &f * &g);
     });
 }
-
-#[bench]
-fn bench_poly_multiplication_huge(b: &mut Bencher) {
-    let poly_ring = PolyRing::adjoint(F1369, "x");
-    let a = poly_ring.bind(poly_ring.from(poly_ring.base_ring().generator()));
-    let x = poly_ring.bind(poly_ring.unknown());
-    let f = (0..=1000).map(|i| x.pow(i) * i as i64).sum::<RingElWrapper<&_>>();
-    let g = (0..=1000).map(|i| x.pow(i) * (1000 - i) as i64 * &a).sum::<RingElWrapper<&_>>();
-    let h = (0..=1000).map(|n| x.pow(n) * &a * ((1000 - n) * n * (n + 1) / 2 + n * (n + 1) * (2 * n + 1) / 6) as i64).sum::<RingElWrapper<&_>>()
-         + (0..1000).map(|n| x.pow(2000 - n) * &a * ((1000 - n) * n * (n + 1) / 2 + n * (n + 1) * (2 * n + 1) / 6) as i64).sum::<RingElWrapper<&_>>();
-    b.iter(|| {
-        assert_eq!(h, &f * &g);
-    });
-}
