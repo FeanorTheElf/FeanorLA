7f477d5bdd9e91ad68c894c15a78bea8b2a4f313
Simon Pohmann <simon@pohmann.de>
2022-06-05 12:08:56 +0100

    Added random_point function for extension fields for elliptic curve

--- src/elliptic_curve/finite_field_curve.rs
+++ src/elliptic_curve/finite_field_curve.rs
@@ -7,9 +7,16 @@ impl<K: FiniteRing + HashableElRing> EllipticCurve<WrappingRing<K>> {
     pub fn random_affine_point<F>(&self, mut rng: F) -> EllipticCurvePoint<WrappingRing<K>>
         where F: FnMut() -> u32
     {
+        self.random_affine_point_extension(self.base_field(), rng)
+    }
+
+    pub fn random_affine_point_extension<L, F>(&self, ext_field: &WrappingRing<L>, mut rng: F) -> EllipticCurvePoint<WrappingRing<L>>
+        where F: FnMut() -> u32, L: FiniteRing + CanonicalEmbeddingInfo<K>
+    {
+        assert!(ext_field.is_field().can_use());
         loop {
-            let x = self.base_field().random_element(&mut rng);
-            if let Some(y) = finite_field_sqrt::sqrt(x.pow(3) + &x * &self.A + &self.B, self.base_field()) {
+            let x = ext_field.random_element(&mut rng);
+            if let Some(y) = finite_field_sqrt::sqrt(x.pow(3) + &x * ext_field.embed(self.base_field(), self.A.clone()) + ext_field.embed(self.base_field(), self.B.clone()), &ext_field) {
                 return EllipticCurvePoint::Affine(x, y);
             }
         }
