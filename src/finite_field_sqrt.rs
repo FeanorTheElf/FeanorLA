839872089397f3c4ea6b31775dd9ff6b800204ff
Simon Pohmann <simon@pohmann.de>
2022-06-13 11:48:49 +0100

    Fixed bug in finite field sqrt

--- src/finite_field_sqrt.rs
+++ src/finite_field_sqrt.rs
@@ -5,16 +5,38 @@ use super::ring_extension::simple_extension::*;
 use std::hash::Hasher;
 use std::collections::hash_map::DefaultHasher;
 
+///
+/// Computes a square root in a finite field of a quadratic residue.
+/// 
+/// # Current approach
+/// 
+/// To find whether the value is a quadratic residue, compute `x^((q - 1)/2)`.
+/// Then x is a quadratic residue if and only if this is not -1, as the multiplicative
+/// group of a finite field is cyclic.
+/// 
+/// To find the root, consider the ring
+/// ```text
+/// Fq[T]/(T^2 - x) ~ Fq x Fq
+/// ```
+/// Now we take random elements y in that ring and hope that the the components in the
+/// CRT representation on the right-hand side have order dividing resp. not dividing
+/// `(q - 1)/2`. In this case, we find that `gcd((T^2 - x), y^((q - 1)/2))` gives the
+/// root of x.
+/// 
 pub fn sqrt<F: FiniteRing + HashableElRing>(x: El<F>, field: F) -> Option<El<F>> {
     assert!(field.is_field().can_use());
     assert!(field.characteristic() != 2);
+    
+    if field.is_zero(&x) {
+        return Some(field.zero());
+    }
 
     let mut hasher = DefaultHasher::new();
     field.hash(&mut hasher, &x);
     let mut rng = oorandom::Rand32::new(hasher.finish());
 
     let n = BigInt::RING.quotient(&(field.size() - 1), &BigInt::from(2)).unwrap();
-    if !field.is_one(&field.pow_big(&x, &n)) {
+    if field.is_neg_one(&field.pow_big(&x, &n)) {
         return None;
     }
     let ring: SimpleRingExtension<_, _, VectorArray<_, 2>> = SimpleRingExtension::new(&field, Vector::from_array([x, field.zero()]), "α");
@@ -39,6 +61,7 @@ fn test_zn_sqrt() {
     let a = field.from_z(42);
     let root_a2 = sqrt(field.pow(&a, 2), &field).unwrap();
     assert!(field.is_eq(&a, &root_a2) || field.is_eq(&field.neg(a), &root_a2));
+    assert!(field.is_zero(&sqrt(field.zero(), &field).unwrap()));
 }
 
 #[test]
